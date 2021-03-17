5ba0a04597534211d14575dc85f3168151cbf504
nturinski <naturins@microsoft.com>
2021-03-16 21:10:11 -0700

    Made a change

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -26,7 +26,7 @@ namespace Company.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized butt."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
